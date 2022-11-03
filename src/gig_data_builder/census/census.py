0b2a17c2afb3c100dffc4161eaf1f1e0e9b00369
nuuuwan <nuuuwan@gmail.com>
2022-11-03 09:49:43 +0530

    fix: gig_data_builder.regions.all_region_id_map_and_lg_basic import BIG; lint

--- src/gig_data_builder/census/census.py
+++ src/gig_data_builder/census/census.py
@@ -4,7 +4,8 @@ from utils import dt, jsonx, tsv
 
 from gig_data_builder._constants import DIR_DATA_CENSUS, DIR_STATSL_DATA
 from gig_data_builder._utils import get_data_index, get_data_list, log
-from gig_data_builder.regions.all_region_id_map_and_lg_basic import get_region_id_index
+from gig_data_builder.regions.all_region_id_map_and_lg_basic import \
+    get_region_id_index
 
 METADATA_TABLES_FILE = os.path.join(DIR_STATSL_DATA, 'tables.json')
 METADATA_FIELDS_FILE = os.path.join(DIR_STATSL_DATA, 'fields.json')
@@ -48,7 +49,7 @@ def build():
             gnd_id = d['entity_id']
             regions = region_id_index.get(gnd_id)
             if regions is None:
-                log.error(f'No region info for gnd: {gnd_id}' )
+                log.error(f'No region info for gnd: {gnd_id}')
                 continue
 
             for region_type in [
