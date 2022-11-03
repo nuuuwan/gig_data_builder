1835e7405362adb2fbae04089f3fa57f562c8592
nuuuwan <nuuuwan@gmail.com>
2022-11-03 13:31:25 +0530

    feat: Clean dir

--- src/gig_data_builder/init_dirs.py
+++ src/gig_data_builder/init_dirs.py
@@ -1,13 +1,13 @@
 import os
 
-from gig_data_builder._constants import (DIR_DATA_CENSUS, DIR_DATA_ELECTIONS,
-                                         DIR_DATA_GEO)
+from gig_data_builder._constants import (DIR_DATA, DIR_DATA_CENSUS,
+                                         DIR_DATA_ELECTIONS, DIR_DATA_GEO)
 
 
 def __build_dirs__():
+    os.system(f'rm -rf {DIR_DATA}/*')
     for dir in [DIR_DATA_GEO, DIR_DATA_CENSUS, DIR_DATA_ELECTIONS]:
-        if not os.path.exists(dir):
-            os.mkdir(dir)
+        os.mkdir(dir)
 
 
 if __name__ == '__main__':
