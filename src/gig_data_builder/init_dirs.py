4fea432a933132c29fe771054c44aad1a7ff701b
nuuuwan <nuuuwan@gmail.com>
2022-11-03 15:03:51 +0530

    lint

--- src/gig_data_builder/init_dirs.py
+++ src/gig_data_builder/init_dirs.py
@@ -1,8 +1,9 @@
 import os
 
+from gig_data_builder._constants import (DIR_DATA, DIR_DATA_CENSUS,
+                                         DIR_DATA_GEO, DIR_DATA_GIG2,
+                                         DIR_DATA_TMP)
 from gig_data_builder._utils import log
-from gig_data_builder._constants import (DIR_DATA, DIR_DATA_TMP, DIR_DATA_CENSUS,
-                                         DIR_DATA_GEO, DIR_DATA_GIG2)
 
 
 def __build_dirs__():
@@ -10,7 +11,6 @@ def __build_dirs__():
     for dir in [DIR_DATA_TMP, DIR_DATA_GEO, DIR_DATA_CENSUS, DIR_DATA_GIG2]:
         os.mkdir(dir)
         log.info(f'Created {dir}')
-
 
 
 if __name__ == '__main__':
