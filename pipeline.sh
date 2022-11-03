6a118a4d7ee34a3727af26c098b7600c92ed1edc
nuuuwan <nuuuwan@gmail.com>
2022-11-03 09:32:53 +0530

    feat: Added lg.tsv

--- pipeline.sh
+++ pipeline.sh
@@ -1,7 +1,13 @@
 function run { python3 src/gig_data_builder/$1; }
 
-# Admin Regions: Ents & Geos
+# country.tsv, province.tsv, district.tsv, dsd.tsv, gnd.tsv
 run regions/admin_regions/admin_regions_basic_and_geo.py
 
-# MOH Regions: Ents & Geos
+# moh.tsv
 run regions/moh_basic_and_region_id_map.py
+
+# lg.tsv
+python src/gig_data_builder/regions/all_region_id_map_and_lg_basic.py
+
+echo '----------------------------------------------------------------'
+ls /Users/nuwan.senaratna/Not.Dropbox/_CODING/data/gig-data
