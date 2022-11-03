83a88bf2ec227a43c974c61ca88f8dca2ee809c3
nuuuwan <nuuuwan@gmail.com>
2022-11-03 09:22:15 +0530

    feat: MOH Regions: Ents & Geos

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,7 @@
 function run { python3 src/gig_data_builder/$1; }
 
-# Admin Regions: Create Ents and Geos
+# Admin Regions: Ents & Geos
 run regions/admin_regions/admin_regions_basic_and_geo.py
+
+# MOH Regions: Ents & Geos
+run regions/moh_basic_and_region_id_map.py
