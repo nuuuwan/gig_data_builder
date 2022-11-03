01791f4b82a2e4817e2919b64f2107208a6cbb71
nuuuwan <nuuuwan@gmail.com>
2022-11-03 13:32:36 +0530

    log: dir names

--- src/gig_data_builder/init_dirs.py
+++ src/gig_data_builder/init_dirs.py
@@ -1,5 +1,6 @@
 import os
 
+from gig_data_builder._utils import log
 from gig_data_builder._constants import (DIR_DATA, DIR_DATA_CENSUS,
                                          DIR_DATA_ELECTIONS, DIR_DATA_GEO)
 
@@ -8,6 +9,8 @@ def __build_dirs__():
     os.system(f'rm -rf {DIR_DATA}/*')
     for dir in [DIR_DATA_GEO, DIR_DATA_CENSUS, DIR_DATA_ELECTIONS]:
         os.mkdir(dir)
+        log.info(f'Created {dir}')
+
 
 
 if __name__ == '__main__':
