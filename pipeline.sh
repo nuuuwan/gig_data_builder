da80bd8a71fd2d2ffd07549b8493cba32c1a601c
nuuuwan <nuuuwan@gmail.com>
2022-11-03 09:41:05 +0530

    feat: added ed.tsv, pd.tsv

--- pipeline.sh
+++ pipeline.sh
@@ -3,6 +3,9 @@ function run { python3 src/gig_data_builder/$1; }
 # country.tsv, province.tsv, district.tsv, dsd.tsv, gnd.tsv
 run regions/admin_regions/admin_regions_basic_and_geo.py
 
+# ed.tsv, pd.tsv
+run elections/elections_basic.py
+
 # moh.tsv
 run regions/moh_basic_and_region_id_map.py
 
