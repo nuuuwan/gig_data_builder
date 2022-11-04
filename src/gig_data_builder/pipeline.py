dd01c6847ffa8832d1b19c22d85aee3cc16dc697
nuuuwan <nuuuwan@gmail.com>
2022-11-04 08:47:01 +0530

    feat: Added testMain

--- src/gig_data_builder/pipeline.py
+++ src/gig_data_builder/pipeline.py
@@ -24,47 +24,56 @@ TEST_MODE = True
 _ = print_title
 
 
-def main():
-    if not TEST_MODE:
-        _("initialize dirs")
-        init_dirs.main()
+def testMain():
+    assert(TEST_MODE)
 
-        _("_tmp/precensus-country.tsv")
-        build_precensus_ent_for_country.main()
-        _("_tmp/precensus-[province|dsd|gnd].tsv")
-        build_precensus_ent_for_admin_regions.main()
-        _("geo/[country|province|dsd|gnd]/*.json")
-        build_geo_for_admin_regions.main()
+    _("_tmp/precensus-pregeo-moh.tsv")
+    moh_basic_and_region_id_map.main()
+
+
+
+def main():
+    _("initialize dirs")
+    init_dirs.main()
+
+    _("_tmp/precensus-country.tsv")
+    build_precensus_ent_for_country.main()
+    _("_tmp/precensus-[province|dsd|gnd].tsv")
+    build_precensus_ent_for_admin_regions.main()
+    _("geo/[country|province|dsd|gnd]/*.json")
+    build_geo_for_admin_regions.main()
 
     _("_tmp/precensus-pregeo-[ed|pd].tsv")
     build_precensus_pregeo_ent_for_election_regions.main()
 
-    if not TEST_MODE:
-        _("_tmp/precensus-pregeo-moh.tsv")
-        moh_basic_and_region_id_map.main()
+    _("_tmp/precensus-pregeo-moh.tsv")
+    moh_basic_and_region_id_map.main()
 
-        _("_tmp/precensus-pregeo-lg.tsv _tmp/precensus-region_id_map.tsv")
-        all_region_id_map_and_lg_basic.main()
+    _("_tmp/precensus-pregeo-lg.tsv _tmp/precensus-region_id_map.tsv")
+    all_region_id_map_and_lg_basic.main()
 
-        _("_tmp/precensus-[ed|pd|moh|lg].tsv")
-        non_admin_region_geo.main()
+    _("_tmp/precensus-[ed|pd|moh|lg].tsv")
+    non_admin_region_geo.main()
 
-        _("census/data.*.tsv")
-        census.main()
+    _("census/data.*.tsv")
+    census.main()
 
-        _("[country|province|dsd|gnd|ed|pd|moh|lg].tsv")
-        expand_regions_with_census_info.main()
+    _("[country|province|dsd|gnd|ed|pd|moh|lg].tsv")
+    expand_regions_with_census_info.main()
 
-        _(
-            "gig2/government-elections-[presidential|parliamentary]"
-            + ".regions-ec.{year}.tsv"
-        )
-        elections_results.main()
+    _(
+        "gig2/government-elections-[presidential|parliamentary]"
+        + ".regions-ec.{year}.tsv"
+    )
+    elections_results.main()
 
-        _("sanity checks")
-        sanity_check_pop_per_elector.main()
-        sanity_check_geos.main()
+    _("sanity checks")
+    sanity_check_pop_per_elector.main()
+    sanity_check_geos.main()
 
 
 if __name__ == '__main__':
-    main()
+    if TEST_MODE:
+        testMain()
+    else:
+        main()
