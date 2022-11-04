2f61aa00c90b943a980ce620b46e76a0ad36a42a
nuuuwan <nuuuwan@gmail.com>
2022-11-04 08:44:33 +0530

    refac: build_precensus_pregeo_ent_for_election_regions.py

---
+++ src/gig_data_builder/regions/build_precensus_pregeo_ent_for_election_regions.py
@@ -0,0 +1,23 @@
+import os
+
+from gig_data_builder._basic import get_basic_data_file
+from gig_data_builder._common import ent_types
+from gig_data_builder._constants import DIR_ELECTIONS
+from gig_data_builder._utils import log
+
+PREFIX = '_tmp/precensus-pregeo-'
+
+
+def build_precensus_pregeo_ent_for_election_regions(region_type):
+    basic_file = get_basic_data_file(PREFIX, region_type)
+    os.system(f'cp {DIR_ELECTIONS}/{region_type}.basic.tsv {basic_file}')
+    log.info(f'Wrote {basic_file}')
+
+
+def main():
+    for region_type in ent_types.ELECTION_REGION_TYPES:
+        build_precensus_pregeo_ent_for_election_regions(region_type)
+
+
+if __name__ == '__main__':
+    main()
