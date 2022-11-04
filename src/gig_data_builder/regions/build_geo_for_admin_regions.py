22e4d152104dcded63bcc296eef27993ca7a03c8
nuuuwan <nuuuwan@gmail.com>
2022-11-04 08:04:25 +0530

    refac: Added logging

--- src/gig_data_builder/regions/build_geo_for_admin_regions.py
+++ src/gig_data_builder/regions/build_geo_for_admin_regions.py
@@ -1,16 +1,20 @@
 from gig_data_builder._common import ent_types
 from gig_data_builder._utils import log
 from gig_data_builder.ground_truth import statsl_utils
-from gig_data_builder.regions._geo import save_geo
+from gig_data_builder.regions._geo import get_geo_dir_for_region, save_geo
 
 
 def build_geo_for_admin_regions(region_type):
     df = statsl_utils.get_df(region_type)
 
-    for d in df.to_dict('records'):
+    d_list = df.to_dict('records')
+    for d in d_list:
         id = statsl_utils.get_id(d, region_type)
         save_geo(region_type, id, d['geometry'])
-    log.info(f'Saved geos for {region_type}')
+
+    n_regions = len(d_list)
+    dir_geo = get_geo_dir_for_region(region_type)
+    log.info(f'Wrote {n_regions} files to {dir_geo}')
 
 
 def main():
