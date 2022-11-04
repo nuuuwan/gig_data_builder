0f8d5abe9229984769931a9cbe44352dd84f21fa
nuuuwan <nuuuwan@gmail.com>
2022-11-04 16:38:10 +0530

    refac: add pd to gnds

---
+++ src/gig_data_builder/ground_truth/pd_utils.py
@@ -0,0 +1,92 @@
+import json
+import os
+
+from utils import tsv
+from utils.cache import cache
+
+from gig_data_builder._common.FuzzySearch import FuzzySearch
+from gig_data_builder._constants import DIR_REGION_ID_MAP
+from gig_data_builder._utils import log
+
+
+def get_gnd_and_pd(d, fs):
+    country_id = 'LK'
+    province_id = fs.search(
+        None,
+        country_id,
+        'province',
+        'name',
+        d['PROVINCE_N'],
+    )
+    district_id = fs.search(
+        'province',
+        province_id,
+        'district',
+        'name',
+        d['DISTRICT_N'],
+    )
+
+    dsd_id = fs.search(
+        'district',
+        district_id,
+        'dsd',
+        'name',
+        d['DSD_N'],
+    )
+
+    gnd_id = fs.search(
+        'dsd',
+        dsd_id,
+        'gnd',
+        'name',
+        d['GND_N'],
+    )
+
+    if gnd_id is None:
+        gnd_id = fs.search(
+            'dsd',
+            dsd_id,
+            'gnd',
+            'name_num',
+            d['GND_NO'],
+        )
+
+    if gnd_id is None:
+        log.error('Could not find gnd_id for: %s', json.dumps(d))
+        return None
+
+    pd_id = fs.search(
+        'province',
+        province_id,
+        'pd',
+        'name',
+        d['Polling_D'],
+    )
+
+    if pd_id is None:
+        log.error('Could not find gnd_id for: %s', json.dumps(d))
+        return None
+
+    return [gnd_id, pd_id]
+
+
+@cache('get_gnd_to_pd', 3600)
+def get_gnd_to_pd():
+    raw_map_file = os.path.join(
+        DIR_REGION_ID_MAP, '00-Data_PD_LA_DSD_Ward_GND.tsv'
+    )
+    raw_map_data_list = tsv.read(raw_map_file)
+
+    fs = FuzzySearch()
+    n = len(raw_map_data_list)
+    gnd_to_pd = {}
+    for i, d in enumerate(raw_map_data_list):
+        x = get_gnd_and_pd(d, fs)
+        if not x:
+            continue
+        [gnd_id, pd_id] = x
+        gnd_to_pd[gnd_id] = pd_id
+        if (i + 1) % 1_000 == 0:
+            log.debug(f'{i + 1}/{n}) get_gnd_to_pd: {gnd_id}')
+
+    return gnd_to_pd
---
+++ src/gig_data_builder/regions/add_pd_to_gnd.py
@@ -0,0 +1,27 @@
+from gig_data_builder import _basic
+from gig_data_builder._utils import log
+from gig_data_builder.ground_truth import pd_utils
+
+
+def add_pd_to_gnd(gnd_to_pd):
+    gnd_data_list = _basic.get_basic_data('_tmp/precensus-', 'gnd')
+    gnd_data_list2 = []
+    n = len(gnd_data_list)
+    n_missing = 0
+    for d in gnd_data_list:
+        pd_id = gnd_to_pd.get(d['gnd_id'], None)
+        if not pd_id:
+            n_missing += 1
+        d['pd_id'] = pd_id
+        gnd_data_list2.append(d)
+    _basic.store_basic_data('_tmp/precensus-', 'gnd', gnd_data_list2)
+    log.warning(f'No PD for {n_missing}/{n} GNDs')
+
+
+def main():
+    gnd_to_pd = pd_utils.get_gnd_to_pd()
+    add_pd_to_gnd(gnd_to_pd)
+
+
+if __name__ == '__main__':
+    main()
