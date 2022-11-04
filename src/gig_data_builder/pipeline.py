7118e36e1318ec1dbc3d86f89c18836803ab46b4
nuuuwan <nuuuwan@gmail.com>
2022-11-04 06:27:10 +0530

    style: shorthand for print_title

--- src/gig_data_builder/pipeline.py
+++ src/gig_data_builder/pipeline.py
@@ -15,42 +15,42 @@ def print_title(text):
     print_line()
     print(text)
     print_line()
-
+_ = print_title
 
 def main():
-    print_title("initialize dirs")
+    _("initialize dirs")
     init_dirs.main()
 
-    print_title("_tmp/precensus-[country|province|dsd|gnd].tsv")
+    _("_tmp/precensus-[country|province|dsd|gnd].tsv")
     admin_regions_basic_and_geo.main()
 
-    print_title("_tmp/precensus-pregeo-[ed|pd].tsv")
+    _("_tmp/precensus-pregeo-[ed|pd].tsv")
     elections_basic.main()
 
-    print_title("_tmp/precensus-pregeo-moh.tsv")
+    _("_tmp/precensus-pregeo-moh.tsv")
     moh_basic_and_region_id_map.main()
 
-    print_title(
+    _(
         "_tmp/precensus-pregeo-lg.tsv _tmp/precensus-region_id_map.tsv"
     )
     all_region_id_map_and_lg_basic.main()
 
-    print_title("_tmp/precensus-[ed|pd|moh|lg].tsv")
+    _("_tmp/precensus-[ed|pd|moh|lg].tsv")
     non_admin_region_geo.main()
 
-    print_title("census/data.*.tsv")
+    _("census/data.*.tsv")
     census.main()
 
-    print_title("[country|province|dsd|gnd|ed|pd|moh|lg].tsv")
+    _("[country|province|dsd|gnd|ed|pd|moh|lg].tsv")
     expand_regions_with_census_info.main()
 
-    print_title(
+    _(
         "gig2/government-elections-[presidential|parliamentary]"
         + ".regions-ec.{year}.tsv"
     )
     elections_results.main()
 
-    print_title("sanity checks")
+    _("sanity checks")
     sanity_check_pop_per_elector.main()
     sanity_check_geos.main()
 
