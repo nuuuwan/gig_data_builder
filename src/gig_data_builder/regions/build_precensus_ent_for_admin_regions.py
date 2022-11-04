5abac109aff2a112b56c0404322542557e8f7d2f
nuuuwan <nuuuwan@gmail.com>
2022-11-04 12:20:29 +0530

    feat: Added election ids; added new prefix for gnd

--- src/gig_data_builder/regions/build_precensus_ent_for_admin_regions.py
+++ src/gig_data_builder/regions/build_precensus_ent_for_admin_regions.py
@@ -1,5 +1,6 @@
 from gig_data_builder._basic import store_basic_data
 from gig_data_builder._common import ent_types
+from gig_data_builder.elections.DISTRICT_TO_ED import DISTRICT_TO_ED
 from gig_data_builder.ground_truth import statsl_utils
 
 
@@ -8,8 +9,11 @@ def expand_region(d, region_type):
     expanded_d['id'] = statsl_utils.get_id(d, region_type)
     expanded_d['name'] = statsl_utils.get_name(d, region_type)
     expanded_d['centroid'] = statsl_utils.get_centroid(d)
+
     expanded_d.update(statsl_utils.get_other_fields_idx(d, region_type))
     expanded_d.update(statsl_utils.get_parent_ids_idx(expanded_d['id']))
+    if region_type in ['district', 'dsd', 'gnd']:
+        expanded_d['ed_id'] = DISTRICT_TO_ED[expanded_d['district_id']]
     return expanded_d
 
 
@@ -26,7 +30,12 @@ def build_precensus_ent_for_admin_regions(region_type):
         key=lambda d: d['id'],
     )
 
-    store_basic_data('_tmp/precensus-', region_type, data_list)
+    if region_type == 'gnd':
+        prefix = '_tmp/precensus-prelg-premoh'
+    else:
+        prefix = '_tmp/precensus-'
+
+    store_basic_data(prefix, region_type, data_list)
 
 
 def main():
