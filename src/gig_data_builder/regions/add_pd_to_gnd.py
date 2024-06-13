b236f32e7b09797a17cbaa04c954b9021c75f7d7
nuuuwan <nuuuwan@gmail.com>
2024-06-13 10:06:04 +0530

    Use get_missing_gnd_id_to_pd_id

--- src/gig_data_builder/regions/add_pd_to_gnd.py
+++ src/gig_data_builder/regions/add_pd_to_gnd.py
@@ -1,11 +1,21 @@
 import os
 
+from utils import JSONFile
+
 from gig_data_builder import _basic
 from gig_data_builder._utils import log
 from gig_data_builder.ground_truth import pd_utils
 
 
+def get_missing_gnd_id_to_pd_id():
+    json_file_path = os.path.join(
+        '_variables', 'missing_gnd_id_to_pd_id.json'
+    )
+    return JSONFile(json_file_path).read()
+
+
 def add_pd_to_gnd(gnd_to_pd):
+    missing_gnd_id_to_pd_id = get_missing_gnd_id_to_pd_id()
     gnd_data_list = _basic.get_basic_data(
         os.path.join('_tmp', 'precensus-'), 'gnd'
     )
@@ -13,7 +23,11 @@ def add_pd_to_gnd(gnd_to_pd):
     n = len(gnd_data_list)
     n_missing = 0
     for d in gnd_data_list:
-        pd_id = gnd_to_pd.get(d['gnd_id'], None)
+        gnd_id = d['gnd_id']
+        pd_id = gnd_to_pd.get(gnd_id, None)
+        if not pd_id:
+            pd_id = missing_gnd_id_to_pd_id.get(gnd_id, None)
+
         if not pd_id:
             n_missing += 1
         d['pd_id'] = pd_id
