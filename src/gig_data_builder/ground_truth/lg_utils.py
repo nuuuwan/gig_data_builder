dceeb20982b71c496dfe07ec98f7839768c0cb90
nuuuwan <nuuuwan@gmail.com>
2025-05-11 10:31:24 +0530

    updated lg logic

--- src/gig_data_builder/ground_truth/lg_utils.py
+++ src/gig_data_builder/ground_truth/lg_utils.py
@@ -4,11 +4,11 @@ from utils import JSONFile
 
 
 def get_lg_id_to_name_and_gnd_to_lg():
-    lg_id_to_name = JSONFile(
-        os.path.join('data_ground_truth', 'lg', 'lg_id_to_lg_name.json')
+    lg_id_to_lg_info = JSONFile(
+        os.path.join("data_ground_truth", "lg", "lg_id_to_lg_info.json")
     ).read()
     gnd_to_lg = JSONFile(
-        os.path.join('data_ground_truth', 'lg', 'gnd_id_to_lg_id.json')
+        os.path.join("data_ground_truth", "lg", "gnd_id_to_lg_id.json")
     ).read()
 
-    return (lg_id_to_name, gnd_to_lg)
+    return (lg_id_to_lg_info, gnd_to_lg)
--- src/gig_data_builder/regions/build_precensus_pregeo_lg.py
+++ src/gig_data_builder/regions/build_precensus_pregeo_lg.py
@@ -4,32 +4,33 @@ from gig_data_builder import _basic
 from gig_data_builder.ground_truth import lg_utils
 
 
-def build_precensus_pregeo_lg(lg_id_to_name):
+def build_precensus_pregeo_lg(lg_id_to_lg_info):
     lg_data_list = []
-    for lg_id, lg_name in sorted(
-        lg_id_to_name.items(),
+    for lg_id, lg_info in sorted(
+        lg_id_to_lg_info.items(),
         key=lambda x: x[0],
     ):
-        district_id_num = lg_id[3:5]
-        district_id = f'LK-{district_id_num}'
+        district_id = lg_info["district_id"]
         province_id = district_id[:4]
         d = {
-            'id': lg_id,
-            'lg_id': lg_id,
-            'name': lg_name,
-            'province_id': province_id,
-            'district_id': district_id,
+            "id": lg_id,
+            "lg_id": lg_id,
+            "name": lg_info["lg_name"],
+            "code": lg_info["lg_code"],
+            "province_id": province_id,
+            "district_id": district_id,
+            "_LEGACY_old_lg_id": lg_info["old_lg_id"],
         }
         lg_data_list.append(d)
     _basic.store_basic_data(
-        os.path.join('_tmp', 'precensus-pregeo-'), 'lg', lg_data_list
+        os.path.join("_tmp", "precensus-pregeo-"), "lg", lg_data_list
     )
 
 
 def main():
-    [lg_id_to_name, _] = lg_utils.get_lg_id_to_name_and_gnd_to_lg()
-    build_precensus_pregeo_lg(lg_id_to_name)
+    [lg_id_to_lg_info, _] = lg_utils.get_lg_id_to_name_and_gnd_to_lg()
+    build_precensus_pregeo_lg(lg_id_to_lg_info)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
