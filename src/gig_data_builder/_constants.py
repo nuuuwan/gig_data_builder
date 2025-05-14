3b5075e4c3c14efcb282ee02343e40cc383c714e
nuuuwan <nuuuwan@gmail.com>
2025-05-14 09:18:45 +0530

    -> DIR_GROUND_TRUTH_DATA

--- src/gig_data_builder/_constants.py
+++ src/gig_data_builder/_constants.py
@@ -24,13 +24,13 @@ DIR_DATA_CENSUS = os.path.join(DIR_DATA, "census")
 DIR_DATA_ENTS = os.path.join(DIR_DATA, "ents")
 
 
-DIR_RAW_DATA = "data_ground_truth"
-DIR_STATSL = os.path.join(DIR_RAW_DATA, "statslmap_selection")
+DIR_GROUND_TRUTH_DATA = "data_ground_truth"
+DIR_STATSL = os.path.join(DIR_GROUND_TRUTH_DATA, "statslmap_selection")
 DIR_STATSL_SHAPE = os.path.join(DIR_STATSL, "app", "shape")
 DIR_STATSL_DATA = os.path.join(DIR_STATSL, "app", "data")
 
-DIR_REGION_ID_MAP = os.path.join(DIR_RAW_DATA, "region_id_map")
-DIR_ELECTIONS = os.path.join(DIR_RAW_DATA, "elections")
-DIR_MOH = os.path.join(DIR_RAW_DATA, "moh")
+DIR_REGION_ID_MAP = os.path.join(DIR_GROUND_TRUTH_DATA, "region_id_map")
+DIR_ELECTIONS = os.path.join(DIR_GROUND_TRUTH_DATA, "elections")
+DIR_MOH = os.path.join(DIR_GROUND_TRUTH_DATA, "moh")
 
-DIR_ELECTIONS_RESULTS = os.path.join(DIR_RAW_DATA, "elections_results")
+DIR_ELECTIONS_RESULTS = os.path.join(DIR_GROUND_TRUTH_DATA, "elections_results")
--- src/gig_data_builder/census/census.py
+++ src/gig_data_builder/census/census.py
@@ -3,26 +3,29 @@ import os
 from utils import JSONFile, String, TSVFile
 
 from gig_data_builder import _basic
-from gig_data_builder._constants import (DIR_DATA_GIG2, DIR_RAW_DATA,
-                                         DIR_STATSL_DATA)
+from gig_data_builder._constants import (
+    DIR_DATA_GIG2,
+    DIR_GROUND_TRUTH_DATA,
+    DIR_STATSL_DATA,
+)
 from gig_data_builder._utils import log
 
-METADATA_TABLES_FILE = os.path.join(DIR_STATSL_DATA, 'tables.json')
-METADATA_FIELDS_FILE = os.path.join(DIR_STATSL_DATA, 'fields.json')
+METADATA_TABLES_FILE = os.path.join(DIR_STATSL_DATA, "tables.json")
+METADATA_FIELDS_FILE = os.path.join(DIR_STATSL_DATA, "fields.json")
 
 
 def get_table_name_to_file_name():
     data_list = TSVFile(
-        os.path.join(DIR_RAW_DATA, 'census', 'census-met.tsv')
+        os.path.join(DIR_GROUND_TRUTH_DATA, "census", "census-met.tsv")
     ).read()
     table_name_to_file_name = {}
     for d in data_list:
-        table_name = d['table_name']
-        m1 = d['measurement1']
-        m2 = d['measurement2']
-        e = d['entity']
-        t = d['time']
-        file_name_only = f'{m1}-{m2}.{e}.{t}.tsv'
+        table_name = d["table_name"]
+        m1 = d["measurement1"]
+        m2 = d["measurement2"]
+        e = d["entity"]
+        t = d["time"]
+        file_name_only = f"{m1}-{m2}.{e}.{t}.tsv"
         table_name_to_file_name[table_name] = os.path.join(
             DIR_DATA_GIG2, file_name_only
         )
@@ -33,22 +36,22 @@ def main():
     metadata_tables = JSONFile(METADATA_TABLES_FILE).read()
     metadata_fields = JSONFile(METADATA_FIELDS_FILE).read()
     gnd_data_index = _basic.get_basic_data_index(
-        os.path.join('_tmp', 'precensus-'), 'gnd'
+        os.path.join("_tmp", "precensus-"), "gnd"
     )
     table_name_to_file_name = get_table_name_to_file_name()
 
     for table_id, table_metadata in metadata_tables.items():
-        field_metadata = metadata_fields[table_metadata['Filename']]
+        field_metadata = metadata_fields[table_metadata["Filename"]]
 
         # Build Table for GNDs
         table_file = os.path.join(
-            DIR_STATSL_DATA, 'var_%s_gnd.json' % (table_id)
+            DIR_STATSL_DATA, "var_%s_gnd.json" % (table_id)
         )
         table_data = JSONFile(table_file).read()
         table_data_list = []
         for gnd_id_num, gnd_table_data in table_data.items():
             d = {}
-            d['entity_id'] = 'LK-%s' % (gnd_id_num)
+            d["entity_id"] = "LK-%s" % (gnd_id_num)
             for field_id, value in sorted(
                 gnd_table_data.items(),
                 key=lambda x: (int)(x[0]),
@@ -63,53 +66,53 @@ def main():
         # Expand for other regions
         parent_to_k_to_v = {}
         for d in table_data_list:
-            gnd_id = d['entity_id']
+            gnd_id = d["entity_id"]
             gnd = gnd_data_index.get(gnd_id)
             if gnd is None:
-                log.error(f'No region info for gnd: {gnd_id}')
+                log.error(f"No region info for gnd: {gnd_id}")
                 continue
 
             for region_type in [
-                'country',
-                'province',
-                'district',
-                'dsd',
-                'ed',
-                'pd',
-                'lg',
-                'moh',
+                "country",
+                "province",
+                "district",
+                "dsd",
+                "ed",
+                "pd",
+                "lg",
+                "moh",
             ]:
-                if region_type == 'country':
-                    parent_id = 'LK'
+                if region_type == "country":
+                    parent_id = "LK"
                 else:
-                    parent_id = gnd.get(region_type + '_id', None)
-                    if parent_id is None or parent_id == '':
+                    parent_id = gnd.get(region_type + "_id", None)
+                    if parent_id is None or parent_id == "":
                         continue
 
                 if parent_id not in parent_to_k_to_v:
                     parent_to_k_to_v[parent_id] = {}
                 for k, v in d.items():
-                    if k == 'entity_id':
+                    if k == "entity_id":
                         continue
                     if k not in parent_to_k_to_v[parent_id]:
                         parent_to_k_to_v[parent_id][k] = 0
                     parent_to_k_to_v[parent_id][k] += v
 
         for parent_id, k_to_v in parent_to_k_to_v.items():
-            table_data_list.append({**{'entity_id': parent_id}, **k_to_v})
+            table_data_list.append({**{"entity_id": parent_id}, **k_to_v})
 
         # Save
         table_data_list = sorted(
             table_data_list,
-            key=lambda d: d['entity_id'],
+            key=lambda d: d["entity_id"],
         )
         n_table_data_list = len(table_data_list)
 
-        table_name = String(table_metadata['Title']).snake
+        table_name = String(table_metadata["Title"]).snake
         table_file = table_name_to_file_name[table_name]
         TSVFile(table_file).write(table_data_list)
-        log.debug(f'Writing {n_table_data_list} rows to {table_file}')
+        log.debug(f"Writing {n_table_data_list} rows to {table_file}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
