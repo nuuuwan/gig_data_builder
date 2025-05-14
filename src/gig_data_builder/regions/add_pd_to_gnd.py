2be91e8889cdfe231fc7ab4c36b6c202f77765b2
nuuuwan <nuuuwan@gmail.com>
2025-05-14 11:11:18 +0530

    log.warning(f"No PD for only if zero PDs

--- src/gig_data_builder/regions/add_pd_to_gnd.py
+++ src/gig_data_builder/regions/add_pd_to_gnd.py
@@ -8,35 +8,34 @@ from gig_data_builder.ground_truth import pd_utils
 
 
 def get_missing_gnd_id_to_pd_id():
-    json_file_path = os.path.join(
-        '_variables', 'missing_gnd_id_to_pd_id.json'
-    )
+    json_file_path = os.path.join("_variables", "missing_gnd_id_to_pd_id.json")
     return JSONFile(json_file_path).read()
 
 
 def add_pd_to_gnd(gnd_to_pd):
     missing_gnd_id_to_pd_id = get_missing_gnd_id_to_pd_id()
     gnd_data_list = _basic.get_basic_data(
-        os.path.join('_tmp', 'precensus-'), 'gnd'
+        os.path.join("_tmp", "precensus-"), "gnd"
     )
     gnd_data_list2 = []
     n = len(gnd_data_list)
     n_missing = 0
     for d in gnd_data_list:
-        gnd_id = d['gnd_id']
+        gnd_id = d["gnd_id"]
         pd_id = gnd_to_pd.get(gnd_id, None)
         if not pd_id:
             pd_id = missing_gnd_id_to_pd_id.get(gnd_id, None)
 
         if not pd_id:
             n_missing += 1
-            
-        d['pd_id'] = pd_id
+
+        d["pd_id"] = pd_id
         gnd_data_list2.append(d)
     _basic.store_basic_data(
-        os.path.join('_tmp', 'precensus-'), 'gnd', gnd_data_list2
+        os.path.join("_tmp", "precensus-"), "gnd", gnd_data_list2
     )
-    log.warning(f'No PD for {n_missing}/{n} GNDs')
+    if n_missing > 0:
+        log.warning(f"No PD for {n_missing}/{n} GNDs")
 
 
 def main():
@@ -44,5 +43,5 @@ def main():
     add_pd_to_gnd(gnd_to_pd)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
