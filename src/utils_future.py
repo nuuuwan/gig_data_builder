e9ba6eee4d4f729b40717cb3ca135bef6a34e010
nuuuwan <nuuuwan@gmail.com>
2025-12-07 06:45:54 +0530

    refactor: improve data writing logic; utilize stringified data for modification checks

--- src/utils_future.py
+++ src/utils_future.py
@@ -1,3 +1,5 @@
+import sys
+
 from utils import Hash, Log
 
 log = Log("utils_future.TSVFile")
@@ -8,24 +10,30 @@ class TSVFile:
         self.file_path = file_path
 
     def is_modified(self, new_data_list):
-        hash_new = Hash.md5(str(new_data_list))
-        hash_old = Hash.md5(str(self.read()))
-        return hash_new != hash_old
+        h_new = Hash.md5(str(new_data_list))
+        h_old = Hash.md5(str(self.read()))
+        return h_new != h_old
+
+    @staticmethod
+    def stringify(data_list):
+        return [{k: str(v) for k, v in d.items()} for d in data_list]
 
     def write(self, data_list):
-        if not self.is_modified(data_list):
-            log.warning(
+        stringified_data_list = TSVFile.stringify(data_list)
+        if not self.is_modified(stringified_data_list):
+            log.debug(
                 f"No changes detected in {self.file_path}. Skipping write."
             )
             return False
         with open(self.file_path, "w", encoding="utf8") as f:
-            if len(data_list) > 0:
-                f.write("\t".join(data_list[0].keys()) + "\n")
-                for d in data_list:
+            if len(stringified_data_list) > 0:
+                f.write("\t".join(stringified_data_list[0].keys()) + "\n")
+                for d in stringified_data_list:
                     f.write("\t".join(map(str, d.values())) + "\n")
 
         n = len(data_list)
         log.info(f"Wrote {n} records to {self.file_path}")
+
         return True
 
     def read(self):
